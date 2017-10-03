0325d9b5599a802440c2d46f2c0f317be8e0bc08
justin <burnphenixos@gmail.com>
2017-10-03 23:49:38 +0200

    home update + Login Page design

---
+++ app/src/main/java/justin_martin/adiilapp/Constant.java
@@ -0,0 +1,10 @@
+package justin_martin.adiilapp;
+
+/**
+ * Created by justin on 03/10/2017.
+ */
+
+public interface Constant {
+
+    public String NETWORK_ERROR = "Il semble y avoir un problème avec votre connexion Internet";
+}
--- app/src/main/java/justin_martin/adiilapp/MainActivity.java
+++ app/src/main/java/justin_martin/adiilapp/MainActivity.java
@@ -1,13 +1,50 @@
 package justin_martin.adiilapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Constant{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkConnexion();
+    }
+
+    public void checkConnexion(){
+        if(isOnline()){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        else {
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Internet Connexion");
+            alertDialog.setMessage(Constant.NETWORK_ERROR);
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Réssayer",new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                    checkConnexion();
+                }
+            } );
+
+            alertDialog.show();
+
+        }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null &&
+                cm.getActiveNetworkInfo().isConnectedOrConnecting();
     }
 }
